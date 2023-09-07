c13aae1c4669cddabec1212963cf5d19e4302914
mrizkyaly <rizkyaly20.ra@gmail.com>
2023-09-07 22:49:39 +0700

    feat : login firebase auth

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import auth from '@react-native-firebase/auth';
+import React, {useState} from 'react';
+import {ActivityIndicator, Image, StyleSheet, Text, View} from 'react-native';
 import {Logo} from '../../assets';
 import {Button, Gap, Inputs} from '../../components';
 import {LoginProps} from '../../config/Types';
@@ -8,44 +9,86 @@ import {fonts} from '../../utils';
 
 const Login: React.FC<LoginProps> = ({navigation}) => {
   const {colors} = useTheme();
-
+  const [loading, setLoading] = useState(false);
+  const [errortext, setErrortext] = useState('');
   const [form, setForm] = useForm({
     email: '',
     password: '',
   });
 
+  console.log('errortext', errortext);
+
   const handleSubmitLogin = () => {
-    navigation.replace('MainApp');
+    setErrortext('');
+    if (!form.email) {
+      console.log('Please fill Email');
+      return;
+    }
+    if (!form.password) {
+      console.log('Please fill Password');
+      return;
+    }
+    setLoading(true);
+    auth()
+      .signInWithEmailAndPassword(form.email, form.password)
+      .then(user => {
+        console.log(user);
+        setTimeout(() => {
+          setLoading(false);
+          navigation.replace('MainApp');
+        }, 2000);
+      })
+      .catch(error => {
+        console.log(error);
+        setLoading(false);
+        if (error.code === 'auth/invalid-email') {
+          setErrortext(error.message);
+        } else if (error.code === 'auth/user-not-found') {
+          setErrortext('No User Found');
+        } else {
+          setErrortext('Please check your email id or password');
+        }
+      });
   };
 
   return (
     <View style={[styles.container, {backgroundColor: colors.neutral.white}]}>
-      <Image source={Logo} />
-      <Gap height={48} />
-      <View>
-        <Text style={[styles.textTitle, {color: colors.text.title}]}>
-          Login Sekarang
-        </Text>
-        <Gap height={24} />
-        <Inputs
-          type="with-title"
-          title="email"
-          placeholder="Masukkan Email anda"
-          value={form.email}
-          onChangeText={value => setForm('email', value)}
+      {loading ? (
+        <ActivityIndicator
+          size="large"
+          color={colors.primary.main}
+          style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
         />
-        <Gap height={12} />
-        <Inputs
-          type="with-title"
-          title="Password"
-          placeholder="Masukkan password anda"
-          value={form.password}
-          onChangeText={value => setForm('password', value)}
-          secureTextEntry
-        />
-      </View>
-      <Gap height={48} />
-      <Button title="Login Sekarang" onPress={handleSubmitLogin} />
+      ) : (
+        <>
+          <Image source={Logo} />
+          <Gap height={48} />
+          <View>
+            <Text style={[styles.textTitle, {color: colors.text.title}]}>
+              Login Sekarang
+            </Text>
+            <Gap height={24} />
+            <Inputs
+              type="with-title"
+              title="email"
+              placeholder="Masukkan Email anda"
+              value={form.email}
+              onChangeText={value => setForm('email', value)}
+            />
+            <Gap height={12} />
+            <Inputs
+              type="with-title"
+              title="Password"
+              placeholder="Masukkan password anda"
+              value={form.password}
+              onChangeText={value => setForm('password', value)}
+              secureTextEntry
+            />
+          </View>
+          <Gap height={48} />
+          <Button title="Login Sekarang" onPress={handleSubmitLogin} />
+        </>
+      )}
     </View>
   );
 };
